17e03eaec3e2dfcc235f8cb1b7072f340d1350e3
Łukasz <43967017+magik9907@users.noreply.github.com>
2021-03-04 17:52:56 +0100

    negative number fix

--- FizzBuzzConsole/Program.cs
+++ FizzBuzzConsole/Program.cs
@@ -8,7 +8,7 @@ namespace FizzBuzzConsole
     {
         static public void Check()
         {
-            Regex regex = new Regex(@"\D");
+            Regex regex = new Regex(@"[^-0-9]+");
             Console.WriteLine("Type : exit; To close application");
             string input = "";
             string message;
@@ -23,9 +23,9 @@ namespace FizzBuzzConsole
                         throw new Exception("not number");
 
                     if (CheckModThree(input))
-                       message = String.Concat(message ,"fizz");
+                        message = String.Concat(message, "fizz");
                     if (CheckModFive(input))
-                       message =  String.Concat(message ,"buzz");
+                        message = String.Concat(message, "buzz");
                 }
                 catch (Exception)
                 {
@@ -44,6 +44,7 @@ namespace FizzBuzzConsole
             int sum = 0;
             foreach (var charElem in charArray)
             {
+                if(charElem != '-')
                 sum += int.Parse(charElem.ToString());
             }
 
@@ -52,6 +53,7 @@ namespace FizzBuzzConsole
 
         static private bool CheckModFive(string value)
         {
+            if (value.CompareTo("0") == 0) return false;
             string lastChar = value.Substring(value.Length - 1);
             return (lastChar == "0" || lastChar == "5") ? true : false;
         }
