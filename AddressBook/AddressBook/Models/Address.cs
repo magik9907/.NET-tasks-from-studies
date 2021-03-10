15b2c7ed58a4f6d5fd4e9a1c894489fce7d8e8c7
Łukasz <43967017+magik9907@users.noreply.github.com>
2021-03-10 19:16:13 +0100

    PS2 AddressBook

---
+++ AddressBook/AddressBook/Models/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AddressBook.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required(ErrorMessage = "pole obowiazkowe")]
+        public string Street { get; set; }
+        [Display(Name = "Kod pocztow")]
+        [StringLength(6)]
+        [Required(ErrorMessage = "pole obowiazkowe")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Miasto")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required(ErrorMessage = "pole obowiazkowe")]
+        public string City { get; set; }
+        [Display(Name = "numer")]
+        [Required(ErrorMessage = "pole obowiazkowe")]
+        public int Number { get; set; }
+
+
+    }
+}
---
+++ FizzBuzz/FizzBuzzConsole/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FizzBuzzConsole
+{
+
+    static class FizzBuzz
+    {
+        static public void Check()
+        {
+            Regex regex = new Regex(@"[^-0-9]+");
+            Console.WriteLine("Type : exit; To close application");
+            string input = "";
+            string message;
+
+            do
+            {
+                message = "";
+                input = Console.ReadLine();
+                try
+                {
+                    if (regex.IsMatch(input))
+                        throw new Exception("not number");
+
+                    if (CheckModThree(input))
+                        message = String.Concat(message, "fizz");
+                    if (CheckModFive(input))
+                        message = String.Concat(message, "buzz");
+                    if (message == "")
+                        message = String.Concat(message, input);
+                }
+                catch (Exception)
+                {
+                    message = "Not valid input";
+                }
+                finally
+                {
+                    Console.WriteLine(message);
+                }
+            } while (input != "exit");
+        }
+
+        static private bool CheckModThree(string value)
+        {
+            char[] charArray = value.ToCharArray();
+            int sum = 0;
+            foreach (var charElem in charArray)
+            {
+                if(charElem != '-')
+                sum += int.Parse(charElem.ToString());
+            }
+
+            return (sum % 3 == 0 && sum != 0) ? true : false;
+        }
+
+        static private bool CheckModFive(string value)
+        {
+            if (value.CompareTo("0") == 0) return false;
+            string lastChar = value.Substring(value.Length - 1);
+            return (lastChar == "0" || lastChar == "5") ? true : false;
+        }
+
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            FizzBuzz.Check();
+        }
+    }
+}
